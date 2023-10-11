edefe7a4ac30765c40ac852652a54e81af3ff937
motome <sigmapinefield15@gmail.com>
2023-10-12 00:47:40 +0900

    1011 2nd before the registration process

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ class User(Model):
         database = db
         table_name = "Users"
 
+
 db.create_tables([User])
 
 
@@ -46,18 +47,48 @@ def after_request(response):
     db.close()
     return response
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/new")
 def new():
     return render_template("second_screen.html")
 
+
 @app.route("/course_recommendation")
 def course_recommendation():
     # RECOMMENDERの第一ページ(仮設)に遷移
     return render_template("recommender_top.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/choose_course")
+def choose_course():
+    return render_template("choose_course.html")
+
+
+@app.route("/confirm_course")
+def confirm_course():
+    chosen_course = request.args.get("course", "")
+    return render_template("confirm_course.html", course=chosen_course)
+
+
+@app.route("/register_course")
+def register_course():
+    chosen_course = request.args.get("course", "")
+    return render_template("register_course.html", course=chosen_course)
+
+
+@app.route("/thank_you")
+def thank_you():
+    return render_template("thank_you.html")
 
 
 # ...（その他のルートと関数）
